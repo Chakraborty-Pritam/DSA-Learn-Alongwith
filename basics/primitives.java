6e75a24792caba30a38308e5c11ae332a9b85726
Pritam Chakraborty <108431206+Chakraborty-Pritam@users.noreply.github.com>
2022-09-04 18:47:22 +0530

    Add files via upload

---
+++ basics/primitives.java
@@ -0,0 +1,15 @@
+package com.pritam;
+
+public class primitives {
+    public static void main(String[] args) {
+        int rollno = 64;
+        char letter = 'r' ;
+        float marks = 98.67f ;
+        double largeDecimalNumbers = 456764.4567;
+        long largeInteger = 34567877898777L;
+        boolean check = false;
+    }
+}
+
+
+
