81f90c3061b6f8c783dcf274f614f370bf5320c5
Pritam Chakraborty <108431206+Chakraborty-Pritam@users.noreply.github.com>
2022-10-08 19:50:19 +0530

    Add files via upload

---
+++ Functions_Methods/greeting.java
@@ -0,0 +1,10 @@
+package com.pritam;
+
+public class greeting {
+    public static void main(String[] args) {
+        greeting();                                // calling out the function
+    }
+    static void greeting() {
+        System.out.println("hello  ");
+    }
+}
